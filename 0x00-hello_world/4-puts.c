b29c6722c834c10e77472643a9d9141db61cf1e8
mrr_moh <mohgarba00@gmail.com>
2022-12-10 11:47:21 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,15 @@
+#!/bin/bash
 #include <stdio.h>
 
 /**
+
 * main - prints a given phrase
-* return : 0 if exited properly, non-zero otherwise
+* return : 0
 */
 
 int main(void)
+
 {
-	puts("\" Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
