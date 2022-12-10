01baa58469fd0818026848770da53e34acab89f8
mrr_moh <mohgarba00@gmail.com>
2022-12-10 08:58:34 -0800

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,24 @@
+#!/bin/bash
+#include <stdio.h>
+
+
+
+/**
+ *
+ * * main - prints a given phrase
+ *
+ * * return : 0
+ *
+ * */
+
+
+
+int main(void)
+
+{
+
+	puts("\" Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
